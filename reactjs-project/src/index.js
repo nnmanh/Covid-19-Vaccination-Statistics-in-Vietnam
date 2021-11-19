bba5191f0d23fae288f0a0879b2b837d7073b2c9
VinceTruong <truonghoangvu2001@gmail.com>
2021-11-19 21:54:30 +0700

    try

--- reactjs-project/src/index.js
+++ reactjs-project/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Tableau from './Tableau';
 
 
 ReactDOM.render(
   <>
     <h1>Welcome to ProSPJ</h1>
+    <Tableau/>
   </>,
   document.getElementById('root')
 );
