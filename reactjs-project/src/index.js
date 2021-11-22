0be3b2a84886bd8e582c2d622121b3dbfc40bd71
VinceTruong <truonghoangvu2001@gmail.com>
2021-11-22 20:05:53 +0700

    update

--- reactjs-project/src/index.js
+++ reactjs-project/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Map from './Map';
 import BarChart from './BarChart';
-import Try from './Try';
+
 
 
 ReactDOM.render(
   <>
     <h1>Welcome to embedded Tableau</h1>
+    
     <Map />
     <BarChart />
 
