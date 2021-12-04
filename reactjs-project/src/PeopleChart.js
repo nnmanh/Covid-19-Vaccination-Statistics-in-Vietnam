13a40f912cf9807877f5b2fedaea849c5079b0ca
Thanh Nguyen <thanhnguyends20@gmail.com>
2021-12-04 23:38:53 +0700

    update dynamic

--- reactjs-project/src/PeopleChart.js
+++ reactjs-project/src/PeopleChart.js
@@ -1,117 +1,158 @@
-import React, {useEffect, useState} from 'react';
-import {Line} from "react-chartjs-2";
+
+import React, { useEffect, useState } from 'react';
+import { Line } from "react-chartjs-2";
+
 
 function PeopleChart() {
   const [date_, setDate] = useState(null)
   const [first_, setFirst] = useState(null)
   const [second_, setSecond] = useState(null)
+
+  const [filteredDate, setfDate] = useState(null)
+  const [filteredFirst, setfFirst] = useState(null)
+  const [filteredSecond, setfSecond] = useState(null)
+
+
   var date = []
   var first = []
   var second = []
-  useEffect (async () => {
+  useEffect(async () => {
     const url = 'https://raw.githubusercontent.com/VinceTruong/Visual_Analysis_Project/main/Data/So_nguoi_tiem.json'
-    const response = await fetch (url);
-    const datapoints = await  response.json()
+    const response = await fetch(url);
+    const datapoints = await response.json()
     for (const objects of datapoints) {
       date.push(objects.date)
-      first.push (objects['first_dose_injected'])
-      second.push (objects['second_dose_injected'])
+      first.push(objects['first_dose_injected'])
+      second.push(objects['second_dose_injected'])
     }
-    setDate (date)
-    setFirst (first)
-    setSecond (second) 
-  }, []); 
-  const [filteredDate, setfDate] = useState (null)
-  const [filteredFirst, setfFirst] = useState(null)
-  const [filteredSecond, setfSecond] = useState (null)
+    setDate(date)
+    setFirst(first)
+    setSecond(second)
+    setfDate(date)
+    setfFirst(first)
+    setfSecond(second)
 
-  function changeDate () {
-      const date_2 = [...date_]
-      const first_2 = [...first_]
-      const second_2 = [...second_]
-      const startdate = document.getElementById ('startdate');
-      const enddate = document.getElementById ('enddate');
-      const indexstartdate = date_2.indexOf (startdate.value)
-      const indexenddate = date_2.indexOf (enddate.value)
+  }, []);
+  function changeDate() {
+    const date_2 = [...date_]
+    const first_2 = [...first_]
+    const second_2 = [...second_]
+    const startdate = document.getElementById('startdate');
+    const enddate = document.getElementById('enddate');
+    const indexstartdate = date_2.indexOf(startdate.value)
+    const indexenddate = date_2.indexOf(enddate.value)
 
-      setfDate(date_2.slice(indexstartdate, indexenddate +1))
-      setfFirst(first_2.slice(indexstartdate, indexenddate+1))
-      setfSecond(second_2.slice(indexstartdate, indexenddate+1))
-    }
-    return (
+    setfDate(date_2.slice(indexstartdate, indexenddate + 1))
+    setfFirst(first_2.slice(indexstartdate, indexenddate + 1))
+    setfSecond(second_2.slice(indexstartdate, indexenddate + 1))
+  }
+  return (
     <div>
       <div>
-      <h1 style ={{textAlign: 'center'}}>COVID-19 INJECTION PROCESS IN VIETNAM</h1>
-      <p style = {{textAlign: 'center'}}>(Unit: People)</p>
-      </div>
-      <div style = {{tabSize: "200", alignItems: "right"}}>
         
-        <input onChange = {changeDate}  type = "date" id = "startdate" defaultValue = '2021-08-18'/>
-        <input onChange =  {changeDate} type =  "date" id = "enddate" defaultValue = '2021-11-18'/>  
       </div>
-      <div style= {{justifyContent: 'center', alignItems: 'center',  width:"90%", height:"350px"}}>
-      <Line
-        data = {{
-          labels : filteredDate,
-          datasets : [
-            {
-              label: "Second Dose Injected",
-              backgroundColor: "#FFC7A1",
-              fill: false,
-              tension: 0.4,
-              borderColor: "orange",
-              borderCapStyle: 'butt',
-              borderDash: [],
-              borderDashOffset: 0.9,
-              borderJoinStyle: 'miter', 
-              pointBorderColor: "orange",
-              pointBackgroundColor: "#fff",
-              pointBorderWidth: 6,
-              pointHoverRadius: 6,
-              pointHoverBackgroundColor: "rgba (75, 192, 192, 1)",
-              pointHoverBorderColor: "rgba (220, 220, 220, 1)",
-              pointRadius: 2,
-              pointHitRadius: 10,
-              data: filteredSecond,
-            },
-            {
-              label: "First Dose Injected",
-              data: filteredFirst,
-              backgroundColor: "#A1D7FF",
-              fill: false,
-              lineTension: 0.1,
-              borderColor: "blue",
-              borderDash: [],
-              borderDashOffset: 0.0,
-              pointBorderColor: "blue",
-              pointBackgroundColor: "#fff",
-              pointBorderWidth: 6,
-              pointHoverRadius: 6,
-              pointHoverBackgroundColor: "rgba (75, 72, 192, 1)",
-              pointHoverBorderColor: "black",
-              pointRadius: 1,
-              pointHitRadius: 10
-              
-            }
-        ],
-        }
-      }
-      options = {{
-        animation : {
-          type: "number",
-          easing : 'linear',
-        },
-        layout : {
-          padding : {
-            top: 20,
-            left: 100
+      <div style = {{textAlign : 'center'}}>
+        <input onChange={changeDate} type="date" id="startdate" defaultValue='2021-08-18' />
+        <input onChange={changeDate} type="date" id="enddate" defaultValue='2021-11-18' />
+      </div>
+      <div>
+        <Line
+          data={{
+            labels: filteredDate,
+            datasets: [
+              {
+                type : "line",
+                label: "Second Dose Injected",
+                backgroundColor: "#b0c7c0",
+                borderColor: "#2f7365",
+                fill: true,
+                tension: 1.0,
+                pointHoverRadius: 6,
+                pointHoverBackgroundColor: "#2f7365",
+                pointHoverBorderColor: "white",
+                pointHitRadius: 10,
+                pointRadius: 1,
+                data: filteredSecond,
+              },
+              {
+                label: "First Dose Injected",
+                data: filteredFirst,
+                backgroundColor: "#d7e3e0",
+                borderColor: "#63974b",
+                fill: true,
+                lineTension: 0.1,
+                pointBackgroundColor: "#63974b",
+                pointHoverRadius: 6,
+                pointHoverBackgroundColor: "#63974b",
+                pointHoverBorderColor: "white",
+                pointRadius: 1,
+                pointHitRadius: 10
+
+              }
+            ],
           }
-        }
-      }}        
-  />
-  </div>
-  </div>
+          }
+          options={{
+            interaction: {
+                intersect: false,
+                mode: 'index',
+            },
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'COVID-19 INJECTION PROCESS IN VIETNAM',
+                    color: 'black',
+                    font: { size: 25, weight: 'bold' }
+                },
+                legend: {
+                    display: true,
+                    reverse: true
+                },
+                tooltip: {
+                    backgroundColor: 'rgb(0,0,0,0.75)',
+                    titleColor: 'white',
+                    yAlign : "center",
+                    reverse: true
+                },
+                datalabels: {
+                    color: 'white'
+                }
+            },
+            responsive: true,
+            scales: {
+                x: {
+                    stacked: true,
+                    ticks: {
+                        font: { weight: "650" }
+                    },
+                    grid: {
+                        display: false,
+                        borderColor: 'grey',
+                    }
+                },
+                y: {
+                    stacked: true,
+                    ticks: {
+                        font: { weight: "bold" }
+                    },
+                    title: {
+                        display: true,
+                        text: 'Doses',
+                        color: 'black',
+                        font: { size: 15, weight: 'bold', lineHeight: 1.2 }
+                    },
+                    grid: {
+                        color: 'darkgrey',
+                        borderDash: [8, 4]
+                    }
+                }
+            }
+        }} 
+        />
+      </div>
+    </div>
+    
   )
 }
-  
+
 export default PeopleChart
--- reactjs-project/src/VaccinationsChart.js
+++ reactjs-project/src/VaccinationsChart.js
@@ -1,115 +1,151 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { Bar } from 'react-chartjs-2'
-import axios from 'axios'
 
-export const VaccinationsChart3 = () => {
-    const [chartdata, setchartdata] = useState()
+const VaccinationsChart3 = () => {
+    const [average, setAverage] = useState(null)
+    const [date, setDate] = useState(null)
+    const [firstDose, setFirst] = useState(null)
+    const [secondDose, setSecond] = useState(null)
 
-    const chart=()=> {
+    const [filteredDate, setfDate] = useState(null)
+    const [filteredFirst, setfFirst] = useState(null)
+    const [filteredSecond, setfSecond] = useState(null)
+    const [filteredAverage, setfAverage] = useState(null)
 
-        var date = []
-        var firstDose = []
-        var secondDose = []
-        var average = []
+    useEffect(async () => {
 
-        axios.get("https://raw.githubusercontent.com/VinceTruong/Visual_Analysis_Project/anhngo/Data/Vaccinations%20per%20day/Vaccinations%20per%20day.json")
-        .then(res =>{
-            for (const dataObj of res.data) {
-                date.push(dataObj.Date)
-                firstDose.push(dataObj['First dose injected'])
-                secondDose.push(dataObj['Second dose injected'])
-                average.push(dataObj['7-day average'])
-            }
-            console.log(res.data)
-            setchartdata({
-                labels:date,
-                datasets:[
-                    {
-                        type: 'line',
-                        label: "7-day average",
-                        data: average,
-                        backgroundColor: 'rgba(0,128,0,1)',
-                        borderColor: 'rgba(0,128,0,1)'
-                    },
-                    {
-                        label: "Second dose injected",
-                        data: secondDose,
-                        backgroundColor:'rgba(77,112,101, 0.8)'
-                    },
-                    {
-                        label: "First dose injected",
-                        data: firstDose,
-                        backgroundColor: 'rgba(119,147,91, 0.8)'
-                    }
-                ]
-                })
-        })
+        var date_ = []
+        var firstDose_ = []
+        var secondDose_ = []
+        var average_ = []
+
+        const url = "https://raw.githubusercontent.com/VinceTruong/Visual_Analysis_Project/main/Data/Vaccinations%20per%20day/Vaccinations%20per%20day3.json"
+        const response = await fetch(url)
+        const data = await response.json()
+        for (const dataObj of data) {
+            date_.push(dataObj['Date'])
+            firstDose_.push(dataObj['First dose injected'])
+            secondDose_.push(dataObj['Second dose injected'])
+            average_.push(dataObj['7-day average'])
+        }
+        setAverage(average_)
+        setFirst(firstDose_)
+        setSecond(secondDose_)
+        setDate(date_)
+
+        setfDate(date_)
+        setfAverage(average_)
+        setfFirst(firstDose_)
+        setfSecond(secondDose_)
+    },
+        []
+    );
+
+    function changeDate() {
+        const date_2 = [...date]
+        const first_2 = [...firstDose]
+        const second_2 = [...secondDose]
+        const average_2 = [...average]
+
+        const startdate = document.getElementById('startdate_');
+        const enddate = document.getElementById('enddate_');
+        const indexstartdate = date_2.indexOf(startdate.value)
+        const indexenddate = date_2.indexOf(enddate.value)
+
+        setfDate(date_2.slice(indexstartdate, indexenddate + 1))
+        setfFirst(first_2.slice(indexstartdate, indexenddate + 1))
+        setfSecond(second_2.slice(indexstartdate, indexenddate + 1))
+        setfAverage(average_2.slice(indexstartdate, indexenddate + 1))
+        console.log(filteredDate)
     }
-
-    useEffect(() => {
-        chart()
-    }, [])
-
     return (
         <div>
-            <Bar data={chartdata}
-                options={{
-                    interaction: {
-                        intersect: false,
-                        mode: 'index',
-                    },
-                    plugins: {
-                        title:{
-                            display: true,
-                            text: 'COVID-19 VACCINE DOSES PER DAY',
-                            color: 'black',
-                            font: {size: 25, weight: 'bold'}
-                        },
-                        legend: {
-                            display: true,
-                            reverse: true
-                         },
-                         tooltip:{
-                             backgroundColor:'rgb(0,0,0,0.75)',
-                             titleColor: 'white',
-                             yAlign: 'bottom',
-                             reverse: true
-                         },
-                         datalabels:{
-                             color: 'white'
-                         }
-                    },
-                    responsive: true,
-                    scales: {
-                        x: {
-                            stacked: true,
-                            ticks: {
-                                font: {weight: "650"}
-                                },
-                            grid:{
-                                display: false,
-                                borderColor: 'grey',
-                                }
+            <div style = {{textAlign: 'center'}}>
+                <input onChange={changeDate} type="date" id="startdate_" defaultValue='2021-08-18' />
+                <input onChange={changeDate} type="date" id="enddate_" defaultValue='2021-11-18' />
+            </div>
+            <div>
+                <Bar
+                    data={{
+                        labels: filteredDate,
+                        datasets: [
+                            {
+                                type: 'line',
+                                label: "7-day average",
+                                data: filteredAverage,
+                                backgroundColor: 'rgba(0,128,0,1)',
+                                borderColor: 'rgba(0,128,0,1)'
                             },
-                        y: {
-                            stacked: true,
-                            ticks: {
-                                font: {weight: "bold"}
-                                },
+                            {
+                                label: "Second dose injected",
+                                data: filteredSecond,
+                                backgroundColor: 'rgba(77,112,101, 0.8)'
+                            },
+                            {
+                                label: "First dose injected",
+                                data: filteredFirst,
+                                backgroundColor: 'rgba(119,147,91, 0.8)'
+                            }
+                        ]
+                    }}
+                    options={{
+                        interaction: {
+                            intersect: false,
+                            mode: 'index',
+                        },
+                        plugins: {
                             title: {
                                 display: true,
-                                text: 'Doses',
+                                text: 'COVID-19 VACCINE DOSES PER DAY',
                                 color: 'black',
-                                font: {size: 15, weight: 'bold', lineHeight: 1.2}
+                                font: { size: 25, weight: 'bold' }
+                            },
+                            legend: {
+                                display: true,
+                                reverse: true
+                            },
+                            tooltip: {
+                                backgroundColor: 'rgb(0,0,0,0.75)',
+                                titleColor: 'white',
+                                yAlign : "center",
+                                reverse: true
+                            },
+                            datalabels: {
+                                color: 'white'
+                            }
+                        },
+                        responsive: true,
+                        scales: {
+                            x: {
+                                stacked: true,
+                                ticks: {
+                                    font: { weight: "650" }
                                 },
-                            grid:{
-                                color: 'darkgrey',
-                                borderDash: [8, 4]
+                                grid: {
+                                    display: false,
+                                    borderColor: 'grey',
+                                }
+                            },
+                            y: {
+                                stacked: true,
+                                ticks: {
+                                    font: { weight: "bold" }
+                                },
+                                title: {
+                                    display: true,
+                                    text: 'Doses',
+                                    color: 'black',
+                                    font: { size: 15, weight: 'bold', lineHeight: 1.2 }
+                                },
+                                grid: {
+                                    color: 'darkgrey',
+                                    borderDash: [8, 4]
                                 }
                             }
                         }
-                    }}/>
+                    }} />
+            </div>
         </div>
     )
 }
-
+export default VaccinationsChart3
